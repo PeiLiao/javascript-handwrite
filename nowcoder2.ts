a66b02374f50e0c60af7c493f8c7b6eaa1d4e7a1
PeiLiao <liaopei1996@outlook.com>
2021-04-18 23:52:01 +0800

    lint

--- nowcoder2.ts
+++ nowcoder2.ts
@@ -1,29 +1,29 @@
-// JZ54 
+// JZ54
 // the first no-repeat character in stringstream
 
 //Init module if you need
-var str = '';
-var chars = []
+var str = "";
+var chars = [];
 function Init() {
   // write code here
 }
 //Insert one char from stringstream
 function Insert(ch) {
   // write code here
-  str += ch
+  str += ch;
   if (chars.includes(ch)) {
-    chars = chars.filter(c => c !== ch)
+    chars = chars.filter((c) => c !== ch);
   } else {
-    chars.push(ch)
+    chars.push(ch);
   }
 }
 //return the first appearence once char in current stringstream
 function FirstAppearingOnce() {
   // write code here
   if (chars.length === 0) {
-    return '#'
+    return "#";
   }
-  return chars[0]
+  return chars[0];
 }
 
 // test
@@ -35,44 +35,99 @@ function FirstAppearingOnce() {
 // input a str no more than 9 characters
 // output all possible and different order of str
 // e.g. ab => [ab,ba]
-function Permutation(str: string) {
-  var results = []
-  str.split('').forEach((s, i) => {
+function Permutation2(str: string) {
+  var results = [str];
+  str.split("").forEach((s, i) => {
     for (var j = i + 1; j < str.length; j++) {
-      var newstr = str;
+      var newstr = str.substring(0, i);
       if (s !== str[j]) {
-        newstr.re = str[j]
-        str[j] = s
-        results.push(newstr)
+        newstr += str[j] + str.substring(i + 1, j) + s + str.substring(j + 1);
+        results.push(newstr);
       }
-
     }
-  })
-  return results
+  });
+  return results;
 }
+
+function Permutation(str: string) {
+  var set = new Set();
+  var dp = [""];
+  str.split("").forEach((s, i) => {
+    dp.map((string) => {
+      string + s;
+    });
+  });
+}
+
 // test
-console.log(Permutation('ac'))
-console.log(Permutation('abc'))
-console.log(Permutation('abcd'))
-console.log(Permutation('abad'))
+// console.log(Permutation("ac"));
+// console.log(Permutation("abc"));
+// console.log(Permutation("abcd"));
+// console.log(Permutation("abad"));
+
+//
+function printMatrix(_matrix) {
+  var results = [];
+  (function print(matrix) {
+    console.log(matrix);
+    if (matrix.length === 0 || matrix[0].length === 0) {
+      return;
+    }
+    if (matrix.length === 1) {
+      results = results.concat(matrix[0]);
+      return;
+    }
+    var innerMatrix = [];
+    results = results.concat(matrix[0]);
+    var len = matrix[0].length;
+    for (var i = 1; i <= matrix.length - 2; i++) {
+      results.push(matrix[i][len - 1]);
+      innerMatrix.push(matrix[i].slice(1, len - 1));
+    }
+
+    results = results.concat(matrix[matrix.length - 1].reverse());
+
+    if (len > 1) {
+      for (var i = matrix.length - 2; i >= 1; i--) {
+        results.push(matrix[i][0]);
+      }
+    }
+
+    print(innerMatrix);
+  })(_matrix);
+  return results;
+}
+
+// console.log(
+//   printMatrix([
+//     [1, 2, 3, 4],
+//     [5, 6, 7, 8],
+//     [9, 10, 11, 12],
+//     [13, 14, 15, 16],
+//   ])
+// );
+
+// console.log(printMatrix([[1], [2], [3], [4], [5]]));
 
 // find the least k numbers of input array
 // O(n) = n*k , if sort input, O(n) =n
 function leastNumbers(input: number[], k) {
-  if (k > input.length) return []
-  var stack = input.slice(0, k).sort()
+  if (k > input.length) return [];
+  var stack = input.slice(0, k).sort();
   for (var i = k; i < input.length; i++) {
     var kk = input[i];
     if (kk < stack[k - 1]) {
       stack.pop();
-      if (k === 1) { stack.push(kk) }
+      if (k === 1) {
+        stack.push(kk);
+      }
       for (var j = 0; j < k - 1; j++) {
         if (stack[j] > kk) {
-          stack.splice(j, 0, kk)
+          stack.splice(j, 0, kk);
           break;
         }
         if (j === k - 2) {
-          stack.push(kk)
+          stack.push(kk);
         }
       }
     }
@@ -81,5 +136,29 @@ function leastNumbers(input: number[], k) {
 }
 
 // test
-console.log(leastNumbers([62, 3, 45, 35, 2, 4, 5, 24, 254, 54, 13, 15, 0, 44, 25], 4))
-console.log(leastNumbers([4, 5, 1, 6, 2, 7, 3, 8], 1))
+// console.log(
+//   leastNumbers([62, 3, 45, 35, 2, 4, 5, 24, 254, 54, 13, 15, 0, 44, 25], 4)
+// );
+// console.log(leastNumbers([4, 5, 1, 6, 2, 7, 3, 8], 1));
+
+function FirstNotRepeatingChar(str) {
+  // write code here
+  var obj = {},
+    index = 0;
+  for (var i of str) {
+    if (obj[i] !== undefined) {
+      obj[i] = -1;
+    } else {
+      obj[i] = index;
+    }
+    index++;
+  }
+  console.log(obj);
+  for (var j in obj) {
+    if (obj[j] !== -1) {
+      return obj[j];
+    }
+  }
+}
+
+console.log(FirstNotRepeatingChar("google"));
--- treeSort.ts
+++ treeSort.ts
@@ -1,7 +1,7 @@
 // 二叉排序树，又叫二叉搜索树
 // 之所以能排序，是因为中序遍历得到一个有序数组
 
-import { flatten, flatten_iter } from './flatten';
+import { flatten, flatten_iter } from "./flatten";
 
 // 之所以能查找，是因为很容易得到第K大的数，而无需完整遍历
 class Tree {
@@ -9,62 +9,62 @@ class Tree {
   constructor(elements: number[]) {
     this.root = null;
     elements.forEach((n) => {
-      this.insert(n)
-    }
-    )
+      this.insert(n);
+    });
   }
 
   print() {
-    this.clear(this.root)
+    this.clear(this.root);
     this.mid_order(this.root);
   }
 
   printInZigzag() {
-    let stack1 = [this.root], stack2 = [];
-    let res = [], index = 0;
+    let stack1 = [this.root],
+      stack2 = [];
+    let res = [],
+      index = 0;
     while (true) {
-      res[index] = []
+      res[index] = [];
       if (index % 2 === 0) {
         while (stack1.length > 0) {
           let p = stack1.pop();
           res[index].push(p.value);
           if (p.left) {
-            stack2.push(p.left)
+            stack2.push(p.left);
           }
           if (p.right) {
-            stack2.push(p.right)
+            stack2.push(p.right);
           }
-
         }
       } else {
         while (stack2.length > 0) {
           let p = stack2.pop();
           res[index].push(p.value);
           if (p.right) {
-            stack1.push(p.right)
+            stack1.push(p.right);
           }
           if (p.left) {
-            stack1.push(p.left)
+            stack1.push(p.left);
           }
         }
       }
       if (stack1.length === 0 && stack2.length === 0) {
         break;
       }
-      index++
+      index++;
     }
-    console.log(res)
-    return res
+    console.log(res);
+    return res;
   }
 
   mid_order(node: TreeNode) {
     if (!node) {
-      return
+      return;
     }
     if (node.left) {
       this.mid_order(node.left);
     }
-    console.log(node.value)
+    console.log(node.value);
     if (node.right) {
       this.mid_order(node.right);
     }
@@ -72,7 +72,7 @@ class Tree {
 
   post_order(node: TreeNode) {
     if (!node) {
-      return
+      return;
     }
     if (node.left) {
       this.post_order(node.left);
@@ -80,11 +80,14 @@ class Tree {
     if (node.right) {
       this.post_order(node.right);
     }
-    console.log(node.value)
+    console.log(node.value);
   }
 
   insert(n: number, node?: TreeNode) {
-    if (!this.root) { this.root = new TreeNode(n); return }
+    if (!this.root) {
+      this.root = new TreeNode(n);
+      return;
+    }
 
     if (!node) {
       node = this.root;
@@ -94,16 +97,15 @@ class Tree {
       if (!node.left) {
         node.left = new TreeNode(n);
       } else {
-        this.insert(n, node.left)
+        this.insert(n, node.left);
       }
     } else {
       if (!node.right) {
         node.right = new TreeNode(n);
       } else {
-        this.insert(n, node.right)
+        this.insert(n, node.right);
       }
     }
-
   }
 
   delete(node: TreeNode) {
@@ -126,7 +128,7 @@ class Tree {
     }
 
     node.value = nextNode.value;
-    return this.delete(nextNode)
+    return this.delete(nextNode);
   }
 
   max(node: TreeNode) {
@@ -134,16 +136,15 @@ class Tree {
     while (nextNode.right) {
       nextNode = nextNode.right;
     }
-    return nextNode.value
+    return nextNode.value;
   }
-
 
   min(node: TreeNode) {
     let nextNode = node;
     while (nextNode.left) {
       nextNode = nextNode.left;
     }
-    return nextNode.value
+    return nextNode.value;
   }
 
   clear(root: TreeNode) {
@@ -152,15 +153,15 @@ class Tree {
     }
 
     if (root.left && root.left.deleted) {
-      root.left = null
+      root.left = null;
     }
 
     if (root.right && root.right.deleted) {
-      root.right = null
+      root.right = null;
     }
 
-    this.clear(root.left)
-    this.clear(root.right)
+    this.clear(root.left);
+    this.clear(root.right);
   }
 
   getNthLargest(node: TreeNode, nth: number): number {
@@ -170,15 +171,15 @@ class Tree {
     }
 
     if (nodes < nth) {
-      return undefined
+      return undefined;
     }
 
     const leftNum = node.left ? this.getNodeNumber(node.left) : 0;
     const extra = nth - leftNum;
     if (extra < 0) {
-      return this.getNthLargest(node.left, nth)
+      return this.getNthLargest(node.left, nth);
     } else if (extra > 2) {
-      return this.getNthLargest(node.right, extra - 1)
+      return this.getNthLargest(node.right, extra - 1);
     } else {
       switch (extra) {
         case 0:
@@ -189,9 +190,7 @@ class Tree {
           return this.min(node.right);
       }
     }
-
   }
-
 
   getNodeNumber(node?: TreeNode) {
     if (!node) {
@@ -200,12 +199,12 @@ class Tree {
     let sum = 0;
     function getNumber(node: TreeNode) {
       if (node) {
-        sum++
-        getNumber(node.left)
-        getNumber(node.right)
+        sum++;
+        getNumber(node.left);
+        getNumber(node.right);
       }
     }
-    getNumber(node)
+    getNumber(node);
     return sum;
   }
 
@@ -216,22 +215,20 @@ class Tree {
         lines[line] = [];
       }
       if (!node) {
-        lines[line].push(0)
-        return
+        lines[line].push(0);
+        return;
       }
-      lines[line].push(node.value)
+      lines[line].push(node.value);
       pre_order(node.left, line + 1);
       pre_order(node.right, line + 1);
-
     }
     pre_order(this.root, 0);
-    console.log(lines)
+    console.log(lines);
   }
 
   showTree() {
-    console.log(JSON.stringify(this.root))
+    console.log(JSON.stringify(this.root));
   }
-
 }
 
 class TreeNode {
@@ -251,10 +248,10 @@ class TreeNode {
   }
 }
 
-const tree = new Tree([12, 15, 8, 6, 7, 3, 1, 2, 3, 5, 9, 2, 5])
+const tree = new Tree([12, 15, 8, 6, 7, 3, 1, 2, 3, 5, 9, 2, 5]);
 // tree.print()
-tree.insert(4)
-tree.insert(14)
+tree.insert(4);
+tree.insert(14);
 // tree.print()
 // console.log('NUM', tree.getNodeNumber(tree.root))
 // console.log('15th', tree.getNthLargest(tree.root, 15))
@@ -268,20 +265,20 @@ tree.insert(14)
 // tree.draw()
 // tree.printInZigzag()
 
-
 // Is arr a PostOrder of a search tree
 // to build a binary search tree and get it's post order, then diff it with the input array
 // cause the tree would be different when using different building method, this function is not exact
 function isPostOrder(arr) {
-  var i = -1, root = null;
+  var i = -1,
+    root = null;
   function post_order(node: TreeNode) {
-    i++
+    i++;
     if (!arr[i]) {
-      console.log(3)
-      return true
+      console.log(3);
+      return true;
     }
     if (!node) {
-      return false
+      return false;
     }
     if (node.left) {
       post_order(node.left);
@@ -290,13 +287,15 @@ function isPostOrder(arr) {
       post_order(node.right);
     }
     if (node.value !== arr[i]) {
-      return false
+      return false;
     }
-
   }
 
   function insert(n: number, node?: TreeNode) {
-    if (!root) { root = new TreeNode(n); return }
+    if (!root) {
+      root = new TreeNode(n);
+      return;
+    }
 
     if (!node) {
       node = root;
@@ -306,63 +305,75 @@ function isPostOrder(arr) {
       if (!node.left) {
         node.left = new TreeNode(n);
       } else {
-        insert(n, node.left)
+        insert(n, node.left);
       }
     } else {
       if (!node.right) {
         node.right = new TreeNode(n);
       } else {
-        insert(n, node.right)
+        insert(n, node.right);
       }
     }
   }
 
-  arr.forEach(e => {
-    insert(e)
+  arr.forEach((e) => {
+    insert(e);
   });
-  return post_order(root)
-
+  return post_order(root);
 }
 
 // console.log(isPostOrder([4, 8, 6, 12, 16, 14, 10]))
 // console.log(isPostOrder([4, 6, 8, 12, 16, 14, 10]))
 
-
 // find the middle number
 function isPostOrder2(arr: number[]) {
-  if (arr.length === 0) { return true }
-  var mid = arr[arr.length - 1], flag = 0, k = -1;
+  if (arr.length === 0) {
+    return true;
+  }
+  var mid = arr[arr.length - 1],
+    flag = 0,
+    k = -1;
   for (var i = 0; i < arr.length - 1; i++) {
     if (arr[i] > mid) {
-      k = i
-      flag = 1
+      k = i;
+      flag = 1;
     }
     if (flag === 1 && arr[i] < mid) {
-      return false
+      return false;
     }
   }
-  return isPostOrder2(arr.slice(0, k + 1)) && isPostOrder2(arr.slice(k, arr.length - 1))
+  return (
+    isPostOrder2(arr.slice(0, k + 1)) &&
+    isPostOrder2(arr.slice(k, arr.length - 1))
+  );
 }
 
 function VerifySquenceOfBST(sequence) {
-  if (sequence.length === 0) { return false }
-  return function verify(arr) {
-    if (arr.length === 0) { return true }
+  if (sequence.length === 0) {
+    return false;
+  }
+  return (function verify(arr) {
+    if (arr.length === 0) {
+      return true;
+    }
 
-    var mid = arr[arr.length - 1], flag = 0, k = -1;
+    var mid = arr[arr.length - 1],
+      flag = 0,
+      k = -1;
     for (var i = 0; i < arr.length - 1; i++) {
       if (arr[i] > mid) {
         if (flag === 0) {
-          k = i
+          k = i;
         }
-        flag = 1
+        flag = 1;
       }
       if (flag === 1 && arr[i] < mid) {
-        return false
+        return false;
       }
-    } console.log(arr, k)
-    return verify(arr.slice(0, k)) && verify(arr.slice(k, arr.length - 1))
-  }(sequence)
+    }
+    console.log(arr, k);
+    return verify(arr.slice(0, k)) && verify(arr.slice(k, arr.length - 1));
+  })(sequence);
 }
 
 // console.log(VerifySquenceOfBST([4, 8, 6, 12, 16, 14, 10]))
@@ -372,54 +383,61 @@ function VerifySquenceOfBST(sequence) {
 // console.log(VerifySquenceOfBST([7, 4, 9, 3, 8, 11, 12, 10]))
 // console.log(isPostOrder2([6, 4, 8]))
 
-
-// find the path, which starts from root, and ends when two nodes get given sum 
+// find the path, which starts from root, and ends when two nodes get given sum
 // it's a search question, too
 // width search first
 function sumPath(pRoot: TreeNode, sum) {
-
-  if (!pRoot) { return [] }
+  if (!pRoot) {
+    return [];
+  }
   var results = [];
   var parr = [{ node: pRoot, r: sum, arr: [] }];
   while (parr.length > 0) {
-    var cur = parr.pop()
+    var cur = parr.pop();
     if (!cur.node) {
       continue;
     }
-    const { node: { left, right, value }, r, arr } = cur;
+    const {
+      node: { left, right, value },
+      r,
+      arr,
+    } = cur;
     if (value < r) {
-      parr.push({ node: left, r: r - value, arr: arr.concat(value) })
-      parr.push({ node: right, r: r - value, arr: arr.concat(value) })
+      parr.push({ node: left, r: r - value, arr: arr.concat(value) });
+      parr.push({ node: right, r: r - value, arr: arr.concat(value) });
     }
 
     if (value === r) {
-      arr.push(value)
-      results.push(arr)
+      arr.push(value);
+      results.push(arr);
     }
-
   }
-  results.sort((a, b) => a - b)
-  return results
+  results.sort((a, b) => b.length - a.length);
+  return results;
 }
-var testtree = new Tree([10, 5, 12, 4, 7])
-testtree.draw()
-console.log(sumPath(testtree.root, 30))
-console.log(sumPath(testtree.root, 22))
-
+var testtree = new Tree([10, 5, 12, 4, 7]);
+testtree.draw();
+console.log(sumPath(testtree.root, 30));
+console.log(sumPath(testtree.root, 22));
 
 // I think it's a search question
 // nowcoder JZ38
 // width search first
 function getDepth(pRoot) {
-  if (!pRoot) { return 0 }
+  if (!pRoot) {
+    return 0;
+  }
   var parr = [pRoot];
   var depth = 0;
 
   while (parr.length > 0) {
-    depth++
-    parr = parr.map(p => [p.left, p.right]).reduce((a, b) => a.concat(b)).filter(p => !!p)
+    depth++;
+    parr = parr
+      .map((p) => [p.left, p.right])
+      .reduce((a, b) => a.concat(b))
+      .filter((p) => !!p);
   }
-  return depth
+  return depth;
 }
 
 // console.log(getDepth(tree.root))
